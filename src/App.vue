49ac6c3a1b8d0ddda13652369cb7f6a07f10a1b6
rajbhatt <rjbhatt110@gmail.com>
2022-06-02 02:46:41 +0530

    upload feature on canvas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- Main Router -->
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  mounted() {},
+};
 </script>
 
 <style>
+/* ---- Font Import ---- */
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap");
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
 }
 </style>
---
+++ src/views/Editor.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login-container">
+    <div class="side-block">
+      <div class="header">
+        <h1>Mannr</h1>
+      </div>
+      <b-form-file
+        v-model="file"
+        :state="Boolean(file)"
+        placeholder="Choose a file or drop it here..."
+        @change="onChange($event)"
+      ></b-form-file>
+      <canvas id="c"></canvas>
+    </div>
+  </div>
+</template>
+
+<script>
+// import router from "../router";
+export default {
+  name: "EditorView",
+  data() {
+    return {
+      file: null,
+      vueCanvas: null,
+    };
+  },
+  mounted() {
+    var c = document.getElementById("c");
+    var ctx = c.getContext("2d");
+    console.log(ctx);
+    this.vueCanvas = ctx;
+  },
+  methods: {
+    onChange(event) {
+      var refer = this;
+      let _URL = window.URL || window.webkitURL;
+      let imageInput = event.target.files[0];
+      let img = new Image();
+      //drawing of the test image - img1
+      img.onload = function () {
+        //draw background image
+        refer.vueCanvas.drawImage(img, 0, 0);
+        //draw a box over the top
+        // ctx.fillStyle = "rgba(200, 0, 0, 0.5)";
+        // ctx.fillRect(0, 0, 500, 500);
+      };
+      img.src = _URL.createObjectURL(imageInput);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
